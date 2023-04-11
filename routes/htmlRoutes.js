789d682ddc4edde74651f9945bab7adbb5adb95b
Nicholas MacDonald <macdonaldn@ufl.edu>
2023-04-11 15:32:43 -0400

    fix schema / build routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,14 @@ const router = require("express").Router();
 const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
 
-router.get("/", ({ session: { isLoggedIn } }, res) => {
-  res.render("index", { isLoggedIn });
+router.get("/", async ({ session: { isLoggedIn } }, res) => {
+  const [rows] = await db.query(`SELECT
+  artist.name AS artist_name,
+  artwork.name AS name,
+  artwork.image AS image
+  FROM artist
+  INNER JOIN artwork ON artist.id=artwork.artist_id;`)
+  res.render("index", { rows, isLoggedIn });
 });
 
 router.get("/login", async (req, res) => {
@@ -19,5 +25,27 @@ router.get("/signup", async (req, res) => {
 router.get("/private", checkAuth, ({ session: { isLoggedIn } }, res) => {
   res.render("protected", { isLoggedIn });
 });
+
+router.get('/work/:id', async (req, res) => {
+  const [[work]] = await db.query(
+    'SELECT * FROM artwork WHERE id=?;',
+    [req.params.id]
+  )
+  res.render('work', {work})
+})
+
+router.get('/artist/:artist', async (req, res) => {
+  const [[rows]] = await db.query(
+    `SELECT
+    artist.name AS artist_name,
+    artwork.name AS name,
+    artwork.image AS image
+    FROM artist
+    INNER JOIN artwork ON artist.id=artwork.artist_id
+    WHERE artist.name=?;`,
+    [req.params.artist]
+  )
+  res.render('index', {rows})
+})
 
 module.exports = router;

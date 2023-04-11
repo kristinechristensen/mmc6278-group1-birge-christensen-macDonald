2dfee115b4f00494df0ff865eb897f839e30c9bb
Kristine Christensen <67657119+kristinechristensen@users.noreply.github.com>
2023-04-11 10:32:36 -0500

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,3 +5,35 @@ CREATE TABLE users (
   username VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(200) NOT NULL
 );
+
+CREATE TABLE artist (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+  name VARCHAR(200) NOT NULL, 
+); 
+
+CREATE TABLE artwork (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  artist_id INT NOT NULL, 
+  name VARCHAR(200) NOT NULL, 
+  image VARCHAR(200) NOT NULL,
+  description TEXT, 
+  medium VARCHAR(200),
+  year INT, 
+  location VARCHAR(100),
+  collection VARCHAR(100),
+  FOREIGN KEY (artist_id)
+    REFERENCES artist(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE favorites (
+   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+   artwork_id INT NOT NULL, 
+   user_id INT NOT NULL, 
+   FOREIGN KEY (artwork_id)
+    REFERENCES artwork(id)
+    ON DELETE CASCADE
+  FOREIGN KEY (user_id)
+    REFERENCES users (id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);

ff9fc9f44348d52f5f65bea7403fd8ac68b72666
Kristine Christensen <christensen@morainevalley.edu>
2023-04-13 14:14:35 -0500

    Updates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
+const db = require('../db')
 
-
-router.get("/", ({ session: { isLoggedIn } }, res) => {
-  res.render("index", { isLoggedIn });
+router.get("/", async ({ session: { isLoggedIn } }, res) => {
+  const [rows] = await db.query(`SELECT
+  artwork.id AS id,
+  artist.name AS artist_name,
+  artwork.name AS name,
+  artwork.image AS image
+  FROM artist
+  INNER JOIN artwork ON artist.id=artwork.artist_id;`)
+  res.render("index", { rows, isLoggedIn });
 });
 
 router.get("/login", async (req, res) => {
@@ -20,5 +27,40 @@ router.get("/signup", async (req, res) => {
 router.get("/private", checkAuth, ({ session: { isLoggedIn } }, res) => {
   res.render("protected", { isLoggedIn });
 });
+
+router.get('/work/:id', async (req, res) => {
+  const [[work]] = await db.query(
+    `SELECT
+    artist.name AS artist_name,
+    artwork.artist_id AS artist_id,
+    artwork.name AS name,
+    artwork.image AS image,
+    artwork.description AS description,
+    artwork.medium AS med,
+    artwork.year AS year,
+    artwork.location AS loc,
+    artwork.collection AS coll
+    FROM artist
+    INNER JOIN artwork ON artist.id=artwork.artist_id
+    WHERE artwork.id=?;`,
+    [req.params.id]
+  )
+  res.render('work', {work})
+})
+
+router.get('/artist/:id', async (req, res) => {
+  const [[rows]] = await db.query(
+    `SELECT
+    artwork.id AS id,
+    artist.name AS artist_name,
+    artwork.name AS name,
+    artwork.image AS image
+    FROM artist
+    INNER JOIN artwork ON artist.id=artwork.artist_id
+    WHERE artist.id=?;`,
+    [req.params.id]
+  )
+  res.render('index', {rows})
+})
 
 module.exports = router;

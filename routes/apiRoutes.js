94ef77ac17f1c02008632db1a8f03035769f9c80
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-04-15 10:52:17 -0600

    sample api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,5 +7,13 @@ router.post("/login", controllers.auth.login);
 router.get("/logout", controllers.auth.logout);
 router.post("/signup", controllers.user.create);
 
+
+router.get('/', function(req, res, next) {
+    request({
+      uri: 'https://api.artic.edu/api/v1/artworks?page=2&limit=100',
+    }).pipe(res);
+  });
+
+
 module.exports = router;
  
